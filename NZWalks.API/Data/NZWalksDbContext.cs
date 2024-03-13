dc9bb57a0abd321fd820a88a172a03ada8229382
Михаел Йорданов <gaws@mail.bg>
2024-03-13 11:43:34 +0200

    Add project files.

---
+++ NZWalks.API/Data/NZWalksDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Data
+{
+    public class  NZWalksDbContext : DbContext
+    {
+        public NZWalksDbContext(DbContextOptions<NZWalksDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Walks> Walks { get; set; }
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Difficulty> Difficulties { get; set; }
+    }
+}
---
+++ NZWalks.API/Models/Domain/Walks.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NZWalks.API.Models.Domain
+{
+    public class Walks
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+
+        public double LengthInKm { get; set; }
+
+        public string? WalkImageUrl { get; set; }
+
+        public Guid RegionId { get; set; }
+        [ForeignKey("RegionId")]
+        public Region Region { get; set; }
+
+        public Guid DifficultyId { get; set; }
+        [ForeignKey("DifficultyId")]
+
+        public Difficulty Difficulty { get; set; }
+
+    }
+}
